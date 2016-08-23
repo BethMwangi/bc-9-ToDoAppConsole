86727e3c41b7e3e1a4fb7cf08a6abf3f0b03f231
BethMwangi <wanjikumwangib@gmail.com>
2016-08-23 15:11:39 +0300

    [setup.py] file contains the setup tools setup packages for the project

---
+++ testing/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Todoapp',
+    version='0.1.0',
+    py_modules=['todo'],
+    install_requires=[
+        'Click',
+        'colorama',
+        'termcolor',
+        'pyfiglet'
+    ],
+    entry_points='''
+        [console_scripts]
+        todo=todo:hello
+    ''',
+)
